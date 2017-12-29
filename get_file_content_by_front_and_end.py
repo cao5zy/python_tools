7cad9ccd53e181c35523d300a236b8a8675a7ce7
Zongying Cao <zongying.cao@hpe.com>
2017-12-29 19:19:26 +0800

    improve codes

--- get_file_content_by_front_and_end.py
+++ get_file_content_by_front_and_end.py
@@ -6,14 +6,11 @@ outfile = sys.argv[4] if len(sys.argv) == 5 else 'output.txt'
 startTag = sys.argv[1]
 endTag = sys.argv[2]
 def main():
-	front = ""
-	end = ""
 	def getPart(str):
 		return str[str.find(startTag) + len(startTag) + 1: str.find(endTag)]
 
-	with open(infile) as fileRead:
-		with open(outfile , 'w') as fileOut:
-			fileOut.write(getPart(fileRead.read()))
+	with open(infile) as fileRead, open(outfile , 'w') as fileOut:
+		fileOut.write(getPart(fileRead.read()))
 
 if __name__ == '__main__':
 	main()
