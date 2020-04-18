14e1acc4d873ff930bdea7b8efc2f78d77fd9443
caozongying <zongying_cao@163.com>
2020-04-18 22:39:25 +0800

    完成了import的解析
    Signed-off-by: caozongying <zongying_cao@163.com>

--- parse_es_import.py
+++ parse_es_import.py
@@ -1,2 +1,12 @@
+import re
+
 def parse_import(line):
-    return ''
+    removed_import = line.replace('import', '')
+    #  print(removed_import)
+    removed_from = re.sub('from.*', '', removed_import)
+    #  print(removed_from)
+    removed_bracket = removed_from.replace('{', '').replace('}', '')
+    #  print(removed_bracket)
+    items = list(map(lambda item:item.strip(), removed_bracket.split(',')))
+    # print(items)
+    return items
--- test_parse_es_import.py
+++ test_parse_es_import.py
@@ -1,4 +1,6 @@
 from parse_es_import import parse_import
 
 def test_parse_import():
-    assert parse_import('') == '1'
+    line = "import { StyleSheet, View, Animated, ScrollView, Easing } from 'react-native';"
+    items =  parse_import(line)
+    assert "StyleSheet" in items
