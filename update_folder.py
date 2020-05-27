c6335cdc0037bc080d9d139645a7327f09420f8b
caozongying <zongying_cao@163.com>
2020-05-27 22:00:50 +0800

    完成了更新处理
    Signed-off-by: caozongying <zongying_cao@163.com>

--- update_folder.py
+++ update_folder.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 
 def get_file_names(dirName):
     fileList = []
@@ -14,6 +15,8 @@ def get_file_names(dirName):
 
 
 def get_file_time(filePath):
+    if not os.path.exists(filePath): return None
+    
     st = os.stat(filePath)
     return st.st_mtime or st.st_ctime
 
@@ -23,10 +26,37 @@ def get_target_file_name(source_path, target_path):
 
     return compose_target_path
 
+def get_target_file_dir(target_file):
+    return os.path.split(target_file)[0]
+
+def get_copy_file(get_target_path):
+    def copy_file(source_file):
+        print(source_file)
+        print(get_target_path(source_file))
+        shutil.copy(source_file, get_target_path(source_file))
+    return copy_file
+
+def ensure_target_dir(get_target_path):
+    def create_target_dir(source_file):
+        target_dir = os.path.split(get_target_path(source_file))[0]
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir)
+
+    return create_target_dir
 if __name__ == '__main__':
     source_path = sys.argv[1]
     target_path = sys.argv[2]
     get_target_path = get_target_file_name(source_path, target_path)
+
+    copy_file = get_copy_file(get_target_path)
+    ensure_dir = ensure_target_dir(get_target_path)
     
-#    list(map(lambda f:print(get_file_time(f)), get_file_names(sys.argv[1])))
-    list(map(lambda f:print(get_target_path(f)), get_file_names(sys.argv[1])))    
+    filter_files = lambda source, target: get_file_time(target) == None or get_file_time(source) > get_file_time(target)
+    
+    transferred_files = filter(lambda f:filter_files(f, get_target_path(f)), get_file_names(sys.argv[1]))
+
+    for source_file in transferred_files:
+        ensure_dir(source_file)
+        copy_file(source_file)
+        
+# test
